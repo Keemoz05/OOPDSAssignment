9b4921bfd08e2ad8cc825b75f434f5113829ba9e
SyedHaqeem <deteralex81@gmail.com>
2025-04-26 19:21:05 +0800

    Added file reading for example.txt

---
+++ robot.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+string filetoread = "examplefile.txt";
+int battlefieldlength = 0;
+int battlefieldwidth = 0;
+int steps = 0;
+int robots = 0;
+
+
+void AnalyseFile(string line){
+    
+    if(line.find("M by N") != string::npos){
+        int pos = line.find(":");
+        int pos2 = line.rfind(" ");
+        string strlength = line.substr(pos+1,line.length());
+        string strwidth = line.substr(pos2,line.length());
+        battlefieldlength = stoi(strlength);
+        battlefieldwidth = stoi(strwidth);
+    }
+    else if(line.find("steps")!= string::npos){
+        int pos = line.find(":");
+        string stringnum = line.substr(pos+1,line.length());
+        steps = stoi(stringnum);
+    }
+    else if(line.find("robots")!= string::npos){
+        int pos = line.find(":");
+        string stringnum = line.substr(pos+1,line.length());
+        robots = stoi(stringnum);
+    }
+    else if(line.find("GenericRobot") != string::npos){
+        //create robot object with Robot robot1(Kidd,3,6)
+    }
+    else {
+        cout << "invalid command ts pmo " << endl;
+    }
+}  
+
+int main(){
+
+    ifstream MyReadFile(filetoread);
+    
+    // Variable to store each line from the file
+    string line;
+
+    // Read each line from the file and print it
+    while (getline(MyReadFile, line)) {
+        // Process each line as needed
+        AnalyseFile(line);
+        cout << line << endl;
+
+    }
+    cout << endl;
+    cout << "Battlefield length = " << battlefieldlength << endl;
+    cout << "Battlefield width = " << battlefieldwidth << endl;
+    cout << "Steps = "<< steps << endl;
+    cout << "Amount of robots = "<< robots << endl;
+}
