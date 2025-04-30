bb5a9b5e2285402433b2e92e6f54d5c2cac28dbe
C2Meng <139868860+C2Meng@users.noreply.github.com>
2025-04-30 11:13:48 +0800

    Update robot.cpp

--- robot.cpp
+++ robot.cpp
@@ -17,7 +17,7 @@ using namespace std;
 
 //     public:
 //     robot(); //define default constructor parameters
-//     robot(type string, name string, locationX int, locationY int){
+//     robot(string type, string name, int locationX, int location Y){
 //         type = robot_type;
 //         name = robot_name;
 //         locationX = robot_locationX;
@@ -25,9 +25,9 @@ using namespace std;
 
 
 //     }
-//     //get_robotInfo(); //member func prototypes
-//     //get_lives();
-//     //isAlive();
+//     //void get_robotInfo(); //member func prototypes
+//     //void get_lives();
+//     //void isAlive();
 
 //};
 
