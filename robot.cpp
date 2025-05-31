3c6b24005058c09a3dcba8671bb39664542c060e
C2Meng <139868860+C2Meng@users.noreply.github.com>
2025-06-01 00:30:58 +0800

    Minor bug fix and QOL changes
    -fix infinite loop error on win con
    -padding for display outputs
    -simple turn counter display
    -typo issues and etc

--- robot.cpp
+++ robot.cpp
@@ -17,6 +17,7 @@ string filetoread = "examplefile.txt";
 int battlefieldlength = 0;
 int battlefieldwidth = 0;
 int steps = 0;
+int turncounter = 1;
 int robotamount = 0;
 int r = 0; //if r > robotamount, dont take anymore generic robots
 ofstream outFile("output.txt", ios::out);
@@ -305,7 +306,7 @@ class ShootingRobot : virtual public Robot{
     bool ShotSuccess = false;
 
     protected:
-        int shells= 10;
+        int shells = 10;
 
     public:
 
@@ -333,6 +334,7 @@ class ShootingRobot : virtual public Robot{
             if(robots[i]->get_locationX() == x && robots[i]->get_locationY() == y){
                 cout << this->robot_name <<" attempts shooting at " << robots[i]->get_name() << "!" << endl;
                 this->use_ammo();
+                cout << this->robot_name << " has " << this->get_shells() << " shells left." << endl;
                 int hit_probability = rand() % 100 + 1; //choose from 0 to 100
                 //int hit_probability = 69;
                 if(hit_probability < 70){
@@ -371,7 +373,7 @@ class ShootingRobot : virtual public Robot{
         }
     }while(!ShotSuccess);
     if(this->get_shells() <= 0){
-            cout << this->get_name() <<  "has ran out of shells and will self destruct!" << endl;
+            cout << this->get_name() <<  " has ran out of shells and will self destruct!" << endl;
             this->decrease_lives();
             grid[robot_locationY][robot_locationX] = '-'; //remove destroyed robot from map, correct the syntax if relevant
             robot_locationX = -1; //dead means at -1
@@ -401,7 +403,7 @@ class SeeingRobot : virtual public Robot { // Aidil
         int Y = robots[randomtarget]->get_locationY();
         if(X != -1 && Y != -1){
         cout << robot_name << " is tracking " << robots[randomtarget]->get_name() << " at " << robots[randomtarget]->get_locationX() << " " << robots[randomtarget]->get_locationY() << endl;
-    
+
     }
     }
 
@@ -442,7 +444,7 @@ class SeeingRobot : virtual public Robot { // Aidil
         } else {
         range = 1;
         }
-        
+
         int dx[] = {-1, 0, 1, 1, 1, 0, -1, -1};
         int dy[] = {-1, -1, -1, 0, 1, 1, 1, 0};
 
@@ -452,7 +454,7 @@ class SeeingRobot : virtual public Robot { // Aidil
                     int a = robot_locationX + dx[i] * step;
                     int b = robot_locationY + dy[i] * step;
             //cout << grid[b][a] << " "  << a << " " << b << endl;
-                
+
             if (a >= 0 && a < battlefieldwidth && b >= 0 && b < battlefieldlength){    //check if a and b is out of bounds,if true, dont look there
             if(grid[b][a] != '-'){
 
@@ -636,7 +638,7 @@ class GenericRobot : public ShootingRobot,public MovingRobot,public SeeingRobot,
             else if (chosenUpgrade == "TrackBot"){
                 canTrack = true;
             }
-        } 
+        }
 
 
     }
@@ -709,10 +711,11 @@ int main(){
         }
         respawnRobot();
         DisplayBattlefield();
- 
-       
+
+
 
       steps -=1;
+      turncounter +=1;
 
       for(int i = 0 ; i < r ; i++){
             if(robots[i]->isAlive()){
@@ -724,7 +727,9 @@ int main(){
               if(bots_left ==1){
                 win_con1 = true;
                 cout << W->get_name() << " has won the game!" << endl;
-                break;
+                exit(0);
+
+
 
 
               }
@@ -754,6 +759,7 @@ int main(){
 
 
       steps -=1;
+      turncounter +=1;
 
       for(int i = 0 ; i < r ; i++){
             if(robots[i]->isAlive()){
@@ -765,7 +771,9 @@ int main(){
               if(bots_left ==1){
                 win_con1 = true;
                 cout << W->get_name() << " has won the game!" << endl;
-                break;
+                exit(0); //to ensure game ends once win condition is met and avoids infinite loops
+
+
 
 
 
@@ -797,6 +805,8 @@ void DisplayBattlefield(){
 
     }
     // Print top X-axis labels
+    cout << endl; //top padding
+    cout << "Turn: " << turncounter << endl;
     cout << "    "; // Padding before column numbers
     for (int i = 0; i < grid[0].size(); i++) {
         cout << setw(3) << (i + 1);
@@ -810,7 +820,9 @@ void DisplayBattlefield(){
             cout << setw(3) << grid[i][j]; // Each cell with fixed width
         }
         cout << endl;
+
     }
+    cout << endl; //bottom padding
 
 
 }
@@ -827,12 +839,12 @@ void SpawnRobots(){
         do {
             X = rand() % battlefieldlength;
             Y = rand() % battlefieldwidth;
-            cout << X  << " " << Y << endl;
-        } while (grid[Y][X] != '-' || X ==0 || Y == 0 || X == battlefieldlength || Y == battlefieldwidth); 
+            //cout << X  << " " << Y << endl; coord check
+        } while (grid[Y][X] != '-' || X ==0 || Y == 0 || X == battlefieldlength || Y == battlefieldwidth);
         robots[r]->set_locationX(X);
         robots[r]->set_locationY(Y);
         cout << endl;
-        cout << robots[r]->get_name() << " Bot has spawned at " << "X:" << X-1 << " Y:"  << Y-1 << "!" << endl; 
+        cout << robots[r]->get_name() << " Bot has spawned at " << "X:" << X-1 << " Y:"  << Y-1 << "!" << endl;
         r++;
 
     }
@@ -855,6 +867,7 @@ void CreateRobotObjects(string type,string name,string X,string Y){ //auto is no
             robots[r]->set_locationY(rand() % battlefieldwidth + 1);
         }
         r++;
+
 
     }
 
@@ -904,7 +917,7 @@ void respawnRobot(){ //when called will loop thru the list of respawning robots 
                 else{
 
                     Deaddead.push_back(R); //if the robot is no longer alive, put it into another vector list
-                    cout << R->get_name() <<  " is eliminated and will no longer respawn!" << endl;
+                    cout << R->get_name() <<  " is eliminated and will no longer respawn!" << endl << endl;;
                     respawnlist.erase(respawnlist.begin()); //remove dead robot from respawnlist
                     }
 
